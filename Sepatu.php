f14ed02d516f048ccf061f830caf554b5378c810
19201117_Alifah Restuningsih <90738802+19201117AlifahRestuningsih@users.noreply.github.com>
2021-10-21 11:46:55 +0700

    Update Sepatu.php

--- Sepatu.php
+++ Sepatu.php
@@ -9,25 +9,25 @@ class Sepatu extends CI_Controller {
 
     public function cetak() {
         $this->load->helper('url');
-		$data = [
+	$data = [
             'nama' => $this->input->post('nama'),
             'nohp' => $this->input->post('nohp'),
             'merk' => $this->input->post('merk'),
             'ukuran' => $this->input->post('ukuran'),
-			'harga' => $this->input->post('harga')
-			];
+	    'harga' => $this->input->post('harga')
+	     ];
 			
          if ($this->input->post('merk') == "Nike"){
 			$data['harga'] = 375000;
-		}elseif ($this->input->post('merk') == "Adidas"){
+	}elseif ($this->input->post('merk') == "Adidas"){
 			$data['harga'] = 300000;
-		}elseif ($this->input->post('merk') == "Kickers"){
+	}elseif ($this->input->post('merk') == "Kickers"){
 			$data['harga'] = 250000;
-  		}elseif ($this->input->post('merk') == "Eigers"){
+  	}elseif ($this->input->post('merk') == "Eigers"){
 			$data['harga'] = 275000;
         }elseif ($this->input->post('merk') == "Bucherri"){
 			$data['harga'] = 400000;			
-		}
+	}
 		
         $this->load->view('view-data-sepatu', $data);
     }
